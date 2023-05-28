54a98b023cddb59f37f09cd3f9f27470982f78ab
mcpepvppackdownloads <134834236+mcpepvppackdownloads@users.noreply.github.com>
2023-05-28 18:20:38 +0300

    script.php

--- script.php
+++ script.php
@@ -2,4 +2,4 @@
 
  src='https://www.hCaptcha.com/1/api.js' async defer></script>
 
-<div class="h-captcha" data-sitekey="027a3ae7-ac3b-4cda-9495-6862df52db2a"></div>en in app
+<div class="h-captcha" data-sitekey="027a3ae7-ac3b-4cda-9495-6862df52db2a"></div>en
